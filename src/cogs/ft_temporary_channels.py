1a1a4ed78c383b3a95ff580d944e3b9808a5aab6
PapyMonkey <exastir@icloud.com>
2022-01-29 06:58:41 +0100

    Stable version : temp channel OK

--- src/cogs/ft_temporary_channels.py
+++ src/cogs/ft_temporary_channels.py
@@ -7,54 +7,79 @@ class tmp_channels(commands.Cog):
 	def __init__(self, client):
 		self.client = client
 
-	tmp_chan_list = []
-	tmp_chan_template = "Salon "
-
 	def get_vowels(self):
 		with open("rsc/settings.json", 'r') as fd:
 			settings = json.load(fd)
 		return settings["vowels_list"]
 
-	# Methode qui permet de creer des salons temporaires en se connectant dans un salon de base bien defini.
-	# A l'avenir, l'utilisateur pourra modifier l'ID de ce salon.
+	def get_parent_chan_list(self, guild):
+		with open("rsc/database.json", 'r') as fd:
+			database = json.load(fd)
+		return database[str(guild.id)]["parent_channels"]
+
+	def get_child_chan_list(self, guild, parent_chan):
+		with open("rsc/database.json", 'r') as fd:
+			database = json.load(fd)
+		return database[str(guild.id)]["parent_channels"][str(parent_chan)]["temporary_channels"]
+
+	def get_template(self, guild, parent_chan):
+		with open("rsc/database.json", 'r') as fd:
+			database = json.load(fd)
+		return database[str(guild.id)]["parent_channels"][str(parent_chan)]["name_template"]
+
+	def put_child_chan_list(self, guild, parent_chan, child_chan):
+		with open("rsc/database.json", 'r') as fd:
+			database = json.load(fd)
+		database[str(guild.id)]["parent_channels"][str(parent_chan)]["temporary_channels"].append(child_chan.id)
+		with open("rsc/database.json", 'w') as fd:
+			json.dump(database, fd, indent=4)
+
+	def rm_child_chan_list(self, guild, parent_chan, child_chan):
+		with open("rsc/database.json", 'r') as fd:
+			database = json.load(fd)
+		database[str(guild.id)]["parent_channels"][str(parent_chan)]["temporary_channels"].remove(child_chan.id)
+		with open("rsc/database.json", 'w') as fd:
+			json.dump(database, fd, indent=4)
+
 	@commands.Cog.listener()
 	async def	on_voice_state_update(self, member, before, after):
-		base_voice = self.client.get_channel(698880918835822644)
-		# Check si, apres voice_state_update, l'utilisateur se trouve encore dans un salon
-		# ET si c'est le salon pere.
-		if (after.channel and member.voice.channel == base_voice):
-			# Definit le nom du channel temporaire en fonction de celui de l'utilisateur.
-			tmp_member_name = member.nick if member.nick else member.name
-			tmp_chan_name_prefix = "de "
-			vowels_list = self.get_vowels()
-			for x in vowels_list:
-				if (x == tmp_member_name[0]):
-					tmp_chan_name_prefix = "d'"
+		try:
+			for parent_chan in self.get_parent_chan_list(member.guild):
+				if (after.channel and member.voice.channel == self.client.get_channel(int(parent_chan))):
+					tmp_member_name = member.nick if member.nick else member.name
+					tmp_chan_name_prefix = "de "
+					for x in self.get_vowels():
+						if (x == tmp_member_name[0]):
+							tmp_chan_name_prefix = "d'"
+							break
+					tmp_chan_name = self.get_template(member.guild, parent_chan) + ' ' + tmp_chan_name_prefix + tmp_member_name
+					new_tmp_chan = await member.guild.create_voice_channel(tmp_chan_name, category = member.voice.channel.category)
+					self.put_child_chan_list(member.guild, parent_chan, new_tmp_chan)
+					await member.move_to(new_tmp_chan)
+					await new_tmp_chan.set_permissions(member,	view_channel = True,
+																manage_channels = True,
+																manage_permissions = True,
+																create_instant_invite = True,
+																connect = True,
+																speak = True,
+																stream = True,
+																use_voice_activation = True,
+																priority_speaker = True,
+																mute_members = True,
+																deafen_members = True,
+																move_members = True)
 					break
-			tmp_chan_name = self.tmp_chan_template + tmp_chan_name_prefix + tmp_member_name
-			tmp_chan_id = await member.guild.create_voice_channel(tmp_chan_name, category = member.voice.channel.category)
-			self.tmp_chan_list.append(tmp_chan_id.id)
-			print(self.tmp_chan_list)
-			await member.move_to(tmp_chan_id)
-			await tmp_chan_id.set_permissions(member,	view_channel = True,
-														manage_channels = True,
-														manage_permissions = True,
-														create_instant_invite = True,
-														connect = True,
-														speak = True,
-														stream = True,
-														use_voice_activation = True,
-														priority_speaker = True,
-														mute_members = True,
-														deafen_members = True,
-														move_members = True)
-		elif (before.channel.id in self.tmp_chan_list and (not after.channel or before.channel.id != after.channel.id)):
-			for id in self.tmp_chan_list:
-				channel = self.client.get_channel(id)
-				if (not channel.members):
-					await channel.delete()
-					self.tmp_chan_list.remove(id)
+				child_chan_list = self.get_child_chan_list(member.guild, parent_chan)
+				if (before.channel.id in child_chan_list and (not after.channel or before.channel.id != after.channel.id)):
+					for id in child_chan_list:
+						channel = self.client.get_channel(id)
+						if (not channel.members):
+							await channel.delete()
+							self.rm_child_chan_list(member.guild, parent_chan, channel)
+							break
 					break
+		except:
+			return
 
 def setup(client):
 	client.add_cog(tmp_channels(client))
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import	discord 
+import discord 
 from discord.ext import commands
 import os
 
@@ -8,21 +8,21 @@ client = commands.Bot(command_prefix = '!')
 
 @client.event
 async def	on_ready():
-	# await client.change_presence(status=discord.Status.idle, activity=discord.Streaming(name='Minecraft', url='https://www.twitch.tv/jackmanifoldtv'))
-	print("\n*-------------------------------------------------------*")
-	print("|\t\t\t\t\t\t\t|")
-	print(f"| {client.user} has been successfully connected to Discord ! |")
-	print("|\t\t\t\t\t\t\t|")
-	print("*-------------------------------------------------------*\n")
+    # await client.change_presence(status=discord.Status.idle, activity=discord.Streaming(name='Minecraft', url='https://www.twitch.tv/jackmanifoldtv'))
+    print("\n*-------------------------------------------------------*")
+    print("|\t\t\t\t\t\t\t|")
+    print(f"| {client.user} has been successfully connected to Discord ! |")
+    print("|\t\t\t\t\t\t\t|")
+    print("*-------------------------------------------------------*\n")
 
 initial_extensions = []
 
 for filename in os.listdir('src/cogs'):
-	if (filename.endswith('.py')):
-		initial_extensions.append("cogs." + filename[:-3])
+    if (filename.endswith('.py')):
+        initial_extensions.append("cogs." + filename[:-3])
 
 if (__name__ == '__main__'):
-	for extension in initial_extensions:
-		client.load_extension(extension)
+    for extension in initial_extensions:
+        client.load_extension(extension)
 
 client.run(token_private)
